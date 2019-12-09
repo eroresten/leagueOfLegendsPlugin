aaa21a36c3445985c4038a05d16679c6624da7cc
Erik Oresten <ekarl@DEVEROR.dev.zervicepoint.local>
2019-12-09 09:20:15 +0100

    Added DropDown.GetSummoner and provider.xml

---
+++ Data sources/DropDown.GetSummoner/DropDown.GetSummoner.psm1
@@ -0,0 +1,39 @@
+﻿function Search($config, $search, $category)
+{   
+    switch($category) {
+        'Europe West'{ $region = 'euw1' }
+        'North America' { $region = 'na1' }
+        'Japan' { $region = 'jp1' }
+        default { $region = 'euw1' }
+    }
+
+    $res = Invoke-RestMethod -Method Get -Uri "https://$region.api.riotgames.com/lol/summoner/v4/summoners/by-name/$search" -Headers @{"X-Riot-Token"=$config.APIKey}
+    
+    $res | Select-Object @{
+       Name="Id";
+       Expression = {
+           $_.accountId
+       }
+    },
+    @{
+        Name="Name";
+        Expression = {
+            $_.name
+        }
+    }
+}
+
+function Validate($config, $search) 
+{
+    return @()
+}
+
+function GetCategories($config) 
+{
+    return @('Europe West','North America')
+}
+
+function GetDefault($config)
+{    
+    
+}
